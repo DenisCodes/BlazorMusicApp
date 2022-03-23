9f5da4856bd0782ae3931a2240acd8958ca737cd
deniscodes <piroko25>
2022-03-23 11:08:05 -0400

    Initial commit

---
+++ Models/music.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp.Models
+{
+    public class Music
+    {
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Composer { get; set; }
+        public string Album { get; set; }
+        
+        public Music(string name, string artist, string composer, string album)
+        {
+            Name = name;
+            Artist = artist;
+            Composer = composer;
+            Album = album;
+        }
+    }
+}
